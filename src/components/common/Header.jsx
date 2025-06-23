5f1dbba83b92534b9039d4c467a4dfa6c405ddc3
jatin-rajput563 <jkt3298@gmail.com>
2025-06-23 10:46:09 +0530

    starting-marque

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
 import { Television } from "../../utils/Icons";
+import { MARQUE_DATA } from "../../utils/helper";
 
 const Header = () => {
   return (
     <>
-      <div className="min-h-screen">
+      <div className="min-h-screen p-[27px]">
         <Marquee>
-          <Television />
+          {MARQUE_DATA.map((item, index) => (
+            <div
+              key={index}
+            className="flex gap-3.5 items-center w-[207px] h-[91px] border border-[#E7EAED] p-4 rounded-lg mx-1.5"
+            >
+              <item.icon />
+              <p className="text-black font-bold leading-[120%]">{item.name}</p>
+            </div>
+          ))}
         </Marquee>
       </div>
     </>
---
+++ src/utils/helper.js
@@ -0,0 +1,76 @@
+import { game, Graphic, Moniters, Smartwatch, Storage, Television } from "./Icons";
+
+export const MARQUE_DATA = [
+    {
+        icon: game,
+        name: "Game",
+    },
+    {
+        icon: Storage,
+        name: "Storage (SSD)",
+    },
+    {
+        icon: Graphic,
+        name: "Graphic Card",
+    },
+    {
+        icon: Television,
+        name: "Televisions",
+    },
+    {
+        icon: Smartwatch,
+        name: "Smartwatch",
+    },
+    {
+        icon: Moniters,
+        name: "Monitors",
+    },
+    {
+        icon: game,
+        name: "Game",
+    },
+    {
+        icon: Storage,
+        name: "Storage (SSD)",
+    },
+    {
+        icon: Graphic,
+        name: "Graphic Card",
+    },
+    {
+        icon: Television,
+        name: "Televisions",
+    },
+    {
+        icon: Smartwatch,
+        name: "Smartwatch",
+    },
+    {
+        icon: Moniters,
+        name: "Monitors",
+    },
+    {
+        icon: game,
+        name: "Game",
+    },
+    {
+        icon: Storage,
+        name: "Storage (SSD)",
+    },
+    {
+        icon: Graphic,
+        name: "Graphic Card",
+    },
+    {
+        icon: Television,
+        name: "Televisions",
+    },
+    {
+        icon: Smartwatch,
+        name: "Smartwatch",
+    },
+    {
+        icon: Moniters,
+        name: "Monitors",
+    },
+]
