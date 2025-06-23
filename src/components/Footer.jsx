317f69b5e0eb14d72b52dddfd7a9662b151d4102
jatin-rajput563 <jkt3298@gmail.com>
2025-06-24 02:01:00 +0530

    work-on-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import footerLogo from "../assets/images/svg/footer-logo.svg";
 import Para from "./common/Para";
+import {
+  FacebookSvg,
+  InstagramSvg,
+  TwiterSvg,
+  YoutubeSvg,
+} from "../utils/Icons";
 
 const Footer = () => {
   return (
@@ -15,21 +21,66 @@ const Footer = () => {
             />
             <ul className="flex gap-2.5 justify-center pt-[27px]">
               <li>
-                <a className="text-white" href="#">Shipping</a>
+                <a className="text-white" href="#">
+                  Shipping
+                </a>
               </li>
               <li>
-                <a className="text-white" href="#">Warranty</a>
+                <a className="text-white" href="#">
+                  Warranty
+                </a>
               </li>
               <li>
-                <a className="text-white" href="#">FAQs</a>
+                <a className="text-white" href="#">
+                  FAQs
+                </a>
               </li>
               <li>
-                <a className="text-white" href="#">Blog</a>
+                <a className="text-white" href="#">
+                  Blog
+                </a>
               </li>
               <li>
-                <a className="text-white" href="#">Contact</a>
+                <a className="text-white" href="#">
+                  Contact
+                </a>
               </li>
             </ul>
+            <div className="flex gap-[10px] justify-center pt-[27px]">
+              <a
+                className="h-[32px] w-[32px] flex justify-center items-center border border-white rounded-full cursor-pointer transform hover:scale-125 hover:rotate-12 transition-transform duration-300 ease-in-out active:scale-90"
+                href="https://www.facebook.com/"
+                target="_blank"
+              >
+                <FacebookSvg />
+              </a>
+              <a
+                href="https://www.instagram.com/"
+                className="h-[32px] w-[32px] flex justify-center items-center border border-white rounded-full cursor-pointer transform hover:scale-125 hover:-rotate-12 transition-transform duration-300 ease-in-out active:scale-90"
+                target="_blank"
+              >
+                <InstagramSvg />
+              </a>
+              <a
+                href="https://x.com/"
+                className="h-[32px] w-[32px] flex justify-center items-center border border-white rounded-full cursor-pointer transform hover:scale-125 hover:rotate-12 transition-transform duration-300 ease-in-out active:scale-90"
+                target="_blank"
+              >
+                <TwiterSvg />
+              </a>
+              <a
+                href="https://www.youtube.com/"
+                className="h-[32px] w-[32px] flex justify-center items-center border border-white rounded-full cursor-pointer transform hover:scale-125 hover:-rotate-12 transition-transform duration-300 ease-in-out active:scale-90"
+                target="_blank"
+              >
+                <YoutubeSvg />
+              </a>
+            </div>
+            <div className="w-full bg-white h-1 mt-[82px]"></div>
+            <div className="flex justify-between max-w-[1440px] mx-auto py-[30px]">
+              <p className="text-sm text-white">term of service | Privacy Policy</p>
+              <p className="text-sm text-white">© 2025 Skaroob. All Rights Reserved.</p>
+            </div>
           </div>
         </div>
       </div>
