d43493ec42d7f423757c8261f20f8abb9c61e079
jatin-rajput563 <jkt3298@gmail.com>
2025-06-24 00:10:48 +0530

    work-on-productDetails-section

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,11 +1,157 @@
-import React from 'react'
+import React, { useState } from "react";
+import RouterImg from "../assets/images/png/product-router-img.png";
+import Heading from "./common/Heading";
+import Para from "./common/Para";
+import Buttons from "./common/Buttons";
+import { FiveStarSvg, MinusSvg, PlusSvg } from "../utils/Icons";
 
 const ProductDetail = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const colors = ["#000000", "#112D49", "#00E676", "#FFFFFF", "#72A4E8"];
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+  const [quantity, setQuantity] = useState(1);
 
-export default ProductDetail
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="max-w-[1140px] mx-auto px-3">
+          <div className="flex flex-wrap flex-row -mx-3">
+            <div className="w-6/12">
+              <div className="py-[44px] px-[42px] bg-[#EEF4FB] max-w-[517px] rounded-[8px]">
+                <img
+                  className="max-w-[432.07px] w-full min-h-[475px]"
+                  src={RouterImg}
+                  alt="Router-Img"
+                />
+              </div>
+              <div className="flex flex-wrap max-w-[517px] gap-[22px] mt-4">
+                <div className="bg-[#EEF4FB] max-w-[156px] rounded-[4px] px-[30px] py-[9px]">
+                  <img
+                    className="max-w-[96px] min-h-[107px]"
+                    src={RouterImg}
+                    alt="router-img"
+                  />
+                </div>
+                <div className="bg-[#EEF4FB] max-w-[156px] rounded-[4px] px-[30px] py-[9px]">
+                  <img
+                    className="max-w-[96px] min-h-[107px]"
+                    src={RouterImg}
+                    alt="router-img"
+                  />
+                </div>
+                <div className="bg-[#EEF4FB] max-w-[156px] rounded-[4px] px-[30px] py-[9px]">
+                  <img
+                    className="max-w-[96px] min-h-[107px]"
+                    src={RouterImg}
+                    alt="router-img"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="w-6/12">
+              <Heading
+                headClass="!text-[34px] text-left !leading-[100%] max-w-[568px]"
+                headText="D-Link ADSL Wireless Router DSL2790U"
+              />
+              <Para
+                paraClass="pt-4 max-w-[568px]"
+                paraText="The D-Link DSL-2790U is a high-speed ADSL2+ wireless router with speeds up to 300 Mbps—ideal for browsing, streaming, and gaming.
+It features four Ethernet ports, strong security, and guest network support. Perfect for reliable internet in homes and small offices"
+              />
+              <Heading
+                headClass="!text-[34px] text-left pt-4"
+                headText="$14.95"
+              />
+              <div className="pt-4">
+                <FiveStarSvg />
+              </div>
+              <div className="flex flex-col items-start gap-4">
+                <Para
+                  paraText="Select Color"
+                  paraClass="!font-semibold pt-[24px]"
+                />
+                <div className="flex items-center gap-[6.52px]">
+                  {colors.map((color, index) => (
+                    <div
+                      key={index}
+                      onClick={() => setSelectedColor(color)}
+                      className={`w-[29px] h-[29px] rounded-full flex items-center justify-center cursor-pointer border ${
+                        color === "#FFFFFF" ? "shadow" : ""
+                      } ${
+                        selectedColor === color
+                          ? "border-gray-400"
+                          : "border-transparent"
+                      }`}
+                      style={{ backgroundColor: color }}
+                    >
+                      {selectedColor === color && (
+                        <svg
+                          width="19"
+                          height="14"
+                          viewBox="0 0 19 14"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M18.4532 1.96968L7.33693 13.2388C7.24012 13.3372 7.12507 13.4154 6.99839 13.4687C6.87172 13.522 6.73591 13.5495 6.59874 13.5495C6.46158 13.5495 6.32577 13.522 6.19909 13.4687C6.07242 13.4154 5.95737 13.3372 5.86055 13.2388L0.997175 8.30853C0.900234 8.21026 0.823337 8.09359 0.770873 7.96519C0.718409 7.83679 0.691406 7.69917 0.691406 7.56019C0.691406 7.42122 0.718409 7.2836 0.770873 7.1552C0.823337 7.0268 0.900234 6.91013 0.997175 6.81186C1.09412 6.71359 1.2092 6.63563 1.33586 6.58245C1.46252 6.52926 1.59827 6.50189 1.73537 6.50189C1.87246 6.50189 2.00821 6.52926 2.13487 6.58245C2.26153 6.63563 2.37662 6.71359 2.47356 6.81186L6.59961 10.9946L16.9786 0.474766C17.1744 0.276295 17.4399 0.164795 17.7168 0.164795C17.9936 0.164795 18.2592 0.276295 18.455 0.474766C18.6507 0.673237 18.7607 0.942422 18.7607 1.2231C18.7607 1.50378 18.6507 1.77297 18.455 1.97144L18.4532 1.96968Z"
+                            fill={
+                              selectedColor === "#FFFFFF" ? "black" : "white"
+                            }
+                          />
+                        </svg>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="flex flex-col items-start gap-4">
+                <Para
+                  paraText="Select Quantity"
+                  paraClass="!font-semibold pt-[35px]"
+                />
+                <div className="flex items-center">
+                  <button
+                    onClick={decreaseQuantity}
+                    className="w-[48px] h-[44px] bg-[#73A4E0] text-white text-xl flex items-center justify-center rounded-l-[8px]"
+                  >
+                    <MinusSvg />
+                  </button>
+                  <div className="w-[85px] h-[44px] border border-[#C2C2C2] text-[#586C80] justify-center flex items-center">
+                    {quantity.toString().padStart(2, "0")}
+                  </div>
+                  <button
+                    onClick={increaseQuantity}
+                    className="w-[48px] h-[44px] bg-[#112D49] text-white text-xl flex items-center justify-center rounded-r-[8px]"
+                  >
+                    <PlusSvg />
+                  </button>
+                </div>
+                <div className="pt-[78px]">
+                  <Buttons
+                    btnClass="!w-full !py-[17px] !px-[246.9px]"
+                    btnText="Buy Now"
+                  />
+                  <Buttons
+                    btnClass="!w-full !py-[17px] !px-[24px] mt-[20px] bg-white !text-[#112D49] hover:!bg-[#112D49] hover:!text-white border border-[#112D49]"
+                    btnText="Add to cart"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDetail;
